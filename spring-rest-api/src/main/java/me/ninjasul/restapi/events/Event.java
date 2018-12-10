b5121aa1193a0543c533deec4ee940b497c652c4
ninjasul@gmail.com <ninjasul@gmail.com>
2018-12-10 19:06:40 +0900

    EventController 생성

--- spring-rest-api/src/main/java/me/ninjasul/restapi/events/Event.java
+++ spring-rest-api/src/main/java/me/ninjasul/restapi/events/Event.java
@@ -15,7 +15,7 @@ public class Event {
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
-    private LocalDateTime cloaseEnrollementDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
     private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
     private String location;
