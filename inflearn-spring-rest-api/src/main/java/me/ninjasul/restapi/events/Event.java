42311145398ce67c6c074a4b1f6ac783332f2962
ninjasul@gmail.com <ninjasul@gmail.com>
2018-12-11 11:01:56 +0900

    EventRepository 생성

--- inflearn-spring-rest-api/src/main/java/me/ninjasul/restapi/events/Event.java
+++ inflearn-spring-rest-api/src/main/java/me/ninjasul/restapi/events/Event.java
@@ -2,6 +2,7 @@ package me.ninjasul.restapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,7 +11,10 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
+
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -24,5 +28,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 }
--- inflearn-spring-rest-api/src/main/java/me/ninjasul/restapi/events/EventController.java
+++ inflearn-spring-rest-api/src/main/java/me/ninjasul/restapi/events/EventController.java
@@ -1,6 +1,7 @@
 package me.ninjasul.restapi.events;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,15 +18,18 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @RequestMapping(value="/api/events", produces= MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
 
+    @Autowired
+    EventRepository eventRepository;
+
     @PostMapping
     public ResponseEntity createEvent(@RequestBody Event event) {
-        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
-        event.setId(10);
+        Event newEvent = eventRepository.save(event);
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 
         log.info("createdUri: {}", createdUri);
-        log.info( "event: {}", event);
+        log.info( "event: {}", newEvent);
 
-        return ResponseEntity.created(createdUri).body(event);
+        return ResponseEntity.created(createdUri).body(newEvent);
     }
 
 }
