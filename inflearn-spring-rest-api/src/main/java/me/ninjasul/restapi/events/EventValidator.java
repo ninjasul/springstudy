dc78d3d6d00bd88fcde62dd88b35614f688c10d2
ninjasul@gmail.com <ninjasul@gmail.com>
2018-12-11 18:16:40 +0900

    ErrorsSerializer 생성

--- inflearn-spring-rest-api/src/main/java/me/ninjasul/restapi/events/EventValidator.java
+++ inflearn-spring-rest-api/src/main/java/me/ninjasul/restapi/events/EventValidator.java
@@ -13,6 +13,8 @@ public class EventValidator {
             eventDto.getMaxPrice() > 0 ) {
             errors.rejectValue( "basePrice", "wrongValue", "BasePrice is wrong");
             errors.rejectValue( "maxPrice", "wrongValue", "MaxPrice is wrong");
+
+            errors.reject("wrongPrices", "Values for prices are wrong");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
