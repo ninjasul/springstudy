9dd693f60f4e4c26eea50724bb792876dd658f7c
ninjasul@gmail.com <ninjasul@gmail.com>
2019-02-07 14:57:54 +0900

    [inflearn-spring-framework2] property

---
+++ inflearn-spring-framework2/_09_property/src/main/java/com/ninjasul/springframework/property/AppRunner.java
@@ -0,0 +1,26 @@
+package com.ninjasul.springframework.property;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("app.properties")
+@Log4j2
+public class AppRunner implements ApplicationRunner {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Environment environment = context.getEnvironment();
+        log.info("app.name: {}", environment.getProperty("app.name"));
+        log.info("app.name2: {}", environment.getProperty("app.name2"));
+    }
+}
