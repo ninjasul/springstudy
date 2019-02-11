7a9d169ffc54dacafce2d48623c531fccfb9c133
ninjasul@gmail.com <ninjasul@gmail.com>
2019-02-11 18:53:17 +0900

    [tobyspring31] 6.4.1 - ProxyFactoryBean

--- tobyspring31/src/test/java/com/ninjasul/tobyspring31/proxy/HelloTest.java
+++ tobyspring31/src/test/java/com/ninjasul/tobyspring31/proxy/HelloTest.java
@@ -1,6 +1,9 @@
 package com.ninjasul.tobyspring31.proxy;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 import java.lang.reflect.Proxy;
 
@@ -31,6 +34,20 @@ public class HelloTest {
         Hello proxiedHello = (Hello) Proxy.newProxyInstance( getClass().getClassLoader(),
                                                             new Class[] { Hello.class },
                                                             new UppercaseHandler(new HelloTarget()));
+
+        assertEquals("HELLO TOBY", proxiedHello.sayHello("Toby"));
+        assertEquals("HI TOBY", proxiedHello.sayHi("Toby"));
+        assertEquals("THANK YOU TOBY", proxiedHello.sayThankYou("Toby"));
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        Hello proxiedHello = (Hello)pfBean.getObject();
 
         assertEquals("HELLO TOBY", proxiedHello.sayHello("Toby"));
         assertEquals("HI TOBY", proxiedHello.sayHi("Toby"));
