a12ea4270a084099a95f0320db83289bfa3b344c
ninjasul <ninjasul@gmail.com>
2019-02-12 23:20:50 +0900

    [tobyspring31] 6.4.2 applied ProxyFactoryBean to UserService

--- tobyspring31/src/main/java/com/ninjasul/tobyspring31/config/AppConfig.java
+++ tobyspring31/src/main/java/com/ninjasul/tobyspring31/config/AppConfig.java
@@ -2,10 +2,16 @@ package com.ninjasul.tobyspring31.config;
 
 import com.ninjasul.tobyspring31.factorybean.Message;
 import com.ninjasul.tobyspring31.factorybean.MessageFactoryBean;
+import com.ninjasul.tobyspring31.user.service.TransactionAdvice;
 import com.ninjasul.tobyspring31.user.service.TxProxyFactoryBean;
 import com.ninjasul.tobyspring31.user.service.UserService;
 import com.ninjasul.tobyspring31.user.service.UserServiceImpl;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +42,7 @@ public class AppConfig {
         return new MessageFactoryBean();
     }
 
+    /*
     @Bean(name="userService")
     public TxProxyFactoryBean userService() {
         log.info("userService()");
@@ -45,5 +52,30 @@ public class AppConfig {
         txProxyFactoryBean.setServiceInterface(UserService.class);
         txProxyFactoryBean.setTransactionManager((PlatformTransactionManager)applicationContext.getBean("transactionManager"));
         return txProxyFactoryBean;
+    }
+    */
+
+    @Bean(name="userService")
+    public ProxyFactoryBean userService() {
+        log.info("userService()");
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(applicationContext.getBean("userServiceTarget"));
+        pfBean.setInterceptorNames("transactionAdvisor");
+        return pfBean;
+    }
+
+    @Bean
+    public Pointcut transactionPointcut() {
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("upgrade*");
+        return pointcut;
+    }
+
+    @Bean
+    public PointcutAdvisor transactionAdvisor() {
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor();
+        advisor.setAdvice((TransactionAdvice)applicationContext.getBean("transactionAdvice"));
+        advisor.setPointcut(transactionPointcut());
+        return advisor;
     }
 }
