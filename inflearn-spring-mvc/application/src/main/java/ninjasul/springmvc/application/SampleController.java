282affe964b45eeba3357a3436e138bdec8dc191
ninjasul@gmail.com <ninjasul@gmail.com>
2019-02-15 17:05:42 +0900

    [inflearn-spring-mvc] URI RequestMapping

---
+++ inflearn-spring-mvc/application/src/main/java/ninjasul/springmvc/application/SampleController.java
@@ -0,0 +1,46 @@
+package ninjasul.springmvc.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SampleController {
+
+    public static final String REQUEST_MAPPING_WITH_HELLO = "RequestMapping with hello";
+    public static final String REQUEST_MAPPING_WITH_A_QUESTION_MARK = "RequestMapping with a question mark";
+    public static final String REQUEST_MAPPING_WITH_A_SUB_PATH = "RequestMapping with a sub path";
+    public static final String REQUEST_MAPPING_WITH_SUB_PATHS = "RequestMapping with sub paths";
+    public static final String REQUEST_MAPPING_WITH_A_REGULAR_EXPRESSION = "RequestMapping with a regular expression";
+
+    @RequestMapping(value="/hello", method={RequestMethod.GET, RequestMethod.PUT})
+    @ResponseBody
+    public String hello() {
+        return REQUEST_MAPPING_WITH_HELLO;
+    }
+
+
+    @GetMapping(value="/hello?")
+    @ResponseBody
+    public String helloWithQuestionMark() {
+        return REQUEST_MAPPING_WITH_A_QUESTION_MARK;
+    }
+
+    @GetMapping(value="/hello/*")
+    @ResponseBody
+    public String helloWithOneSubPath() {
+        return REQUEST_MAPPING_WITH_A_SUB_PATH;
+    }
+
+    @GetMapping(value="/welcome/**")
+    @ResponseBody
+    public String welcomeWithSubPaths() {
+        return REQUEST_MAPPING_WITH_SUB_PATHS;
+    }
+
+    @GetMapping(value="/greetings/{name:[a-z]+}")
+    @ResponseBody
+    public String greetingsWithRegularExpression(@PathVariable String name) {
+        return REQUEST_MAPPING_WITH_A_REGULAR_EXPRESSION + " " + name;
+    }
+
+}
---
+++ inflearn-spring-mvc/application/src/test/java/ninjasul/springmvc/application/SampleControllerTest.java
@@ -0,0 +1,136 @@
+package ninjasul.springmvc.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static ninjasul.springmvc.application.SampleController.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    public void hello_get() throws Exception {
+
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_HELLO));
+
+        mockMvc.perform(put("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_HELLO));
+
+        mockMvc.perform(post("/hello"))
+                .andDo(print())
+                .andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void requestMapping_withQuestionMark() throws Exception {
+
+        mockMvc.perform(get("/hello2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_QUESTION_MARK));
+
+        mockMvc.perform(get("/hello3"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_QUESTION_MARK));
+
+        mockMvc.perform(get("/hello4"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_QUESTION_MARK));
+
+        mockMvc.perform(get("/hello55"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(get("/hello666"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void requestMapping_withOneSubPath() throws Exception {
+
+        mockMvc.perform(get("/hello/world"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_SUB_PATH));
+
+        mockMvc.perform(get("/hello/ninjasul"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_SUB_PATH));
+
+        mockMvc.perform(get("/hello/world/ninjasul"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(get("/hello/world/ninjasul/ok"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void requestMapping_withSubPaths() throws Exception {
+
+        mockMvc.perform(get("/welcome"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_SUB_PATHS));
+
+        mockMvc.perform(get("/welcome/ninjasul"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_SUB_PATHS));
+
+        mockMvc.perform(get("/welcome/world/ninjasul"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_SUB_PATHS));
+
+        mockMvc.perform(get("/welcome/world/ninjasul/ok"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_SUB_PATHS));
+
+
+    }
+
+    @Test
+    public void requestMapping_withRegularExpression() throws Exception {
+
+        mockMvc.perform(get("/greetings/world"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_REGULAR_EXPRESSION + " world"));
+
+        mockMvc.perform(get("/greetings/ninjasul"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(REQUEST_MAPPING_WITH_A_REGULAR_EXPRESSION + " ninjasul"));
+
+    }
+
+
+
+}
