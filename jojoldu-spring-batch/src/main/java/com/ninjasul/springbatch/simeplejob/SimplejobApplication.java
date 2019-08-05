944f762230a5c866e34096c7cfc7028633b9e316
ninjasul@gmail.com <ninjasul@gmail.com>
2019-08-05 18:28:18 +0900

    changed application name

--- jojoldu-spring-batch/src/main/java/com/ninjasul/springbatch/simeplejob/SimplejobApplication.java
+++ jojoldu-spring-batch/src/main/java/com/ninjasul/springbatch/simeplejob/SimplejobApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableBatchProcessing
 @SpringBootApplication
-public class SimeplejobApplication {
+public class SimplejobApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimeplejobApplication.class, args);
+        SpringApplication.run(SimplejobApplication.class, args);
     }
 
 }
