482d00acfca52660ddf86e3243fcaf0f31cfdb3a
ninjasul@gmail.com <ninjasul@gmail.com>
2019-08-07 15:59:51 +0900

    writers

---
+++ jojoldu-spring-batch/src/main/java/com/ninjasul/springbatch/writer/job/CustomItemWriterJobConfiguration.java
@@ -0,0 +1,70 @@
+package com.ninjasul.springbatch.writer.job;
+
+import com.ninjasul.springbatch.reader.domain.Pay;
+import com.ninjasul.springbatch.writer.domain.Pay2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class CustomItemWriterJobConfiguration {
+    private static final int chunkSize = 10;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Job customItemWriterJob() {
+        return jobBuilderFactory.get("customItemWriterJob")
+                .start(customItemWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step customItemWriterStep() {
+        return stepBuilderFactory.get("customItemWriterStep")
+                .<Pay, Pay2>chunk(chunkSize)
+                .reader(customItemWriterReader())
+                .processor(customItemWriterProcessor())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> customItemWriterReader() {
+        return new JpaPagingItemReaderBuilder<Pay>()
+                .name("customItemWriterReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("SELECT p FROM Pay p")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Pay, Pay2> customItemWriterProcessor() {
+        return pay -> new Pay2(pay.getAmount(), pay.getTxName(), pay.getTxDateTime());
+    }
+
+    @Bean
+    public ItemWriter<Pay2> customItemWriter() {
+        return items -> {
+            for (Pay2 item : items) {
+                System.out.println(item);
+            }
+        };
+    }
+}
