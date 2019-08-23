c34b81aae5d99dc204f5e5dcf307ed9bb8fd3850
ninjasul@gmail.com <ninjasul@gmail.com>
2019-08-23 15:28:14 +0900

    modified module name from inflearn-spring-jpa2 -> inflearn-spring-jpa

--- inflearn-spring-jpa/src/main/java/com/inflearn/spring/jpa/InflearnSpringJpaApplication.java
+++ inflearn-spring-jpa/src/main/java/com/inflearn/spring/jpa/InflearnSpringJpaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InflearnSpringJpa2Application {
+public class InflearnSpringJpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InflearnSpringJpa2Application.class, args);
+        SpringApplication.run(InflearnSpringJpaApplication.class, args);
     }
 
 }
