24a3343e1b6f27a1b4b2f09617854474efab07ee
ninjasul@gmail.com <ninjasul@gmail.com>
2019-08-23 14:30:37 +0900

    Initial Commit

---
+++ inflearn-spring-jpa/src/main/java/com/inflearn/spring/jpa/InflearnSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringJpaApplication.class, args);
+    }
+
+}
