e7a9114b540dd900f3483acd6825637428ba599d
1000141 <ninjasul@onestorecorp.com>
2020-04-10 08:29:36 +0900

    inflearn-core-spring-security initial-commit

---
+++ inflearn-core-spring-security/src/main/java/com/ninjasul/corespringsecurity/InflearnCoreSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ninjasul.corespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnCoreSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnCoreSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ inflearn-core-spring-security/src/test/java/com/ninjasul/corespringsecurity/InflearnCoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjasul.corespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnCoreSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
